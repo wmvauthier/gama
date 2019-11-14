913ebce93876228909ed4c32685c5cf333889402
devlinkcne <linkne2012@gmail.com>
2019-11-14 11:41:39 -0300

    Next Task: Estudo sobre IPs e Terminais

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -25,6 +25,27 @@ module.exports.url = function (application, req, res) {
     var url = qrcode.url;
 
     var data = getDataFromNFCe(url);
+
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                return;
+            }
+
+            if (alias >= 1) {
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                console.log(ifname, iface.address);
+            }
+            ++alias;
+        });
+    });
+
     res.send(data);
 
 }
